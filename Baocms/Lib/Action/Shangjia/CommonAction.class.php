96cfb0a88137ca3b840573d30f4b5e106ad9c01e
jiangjun <851732372@qq.com>
2018-10-24 21:11:59 +0800

    手机端自动登录

--- Baocms/Lib/Action/Shangjia/CommonAction.class.php
+++ Baocms/Lib/Action/Shangjia/CommonAction.class.php
@@ -17,6 +17,14 @@ class CommonAction extends Action {
         if (!empty($this->uid)) {
             $this->member = D('Shop')->find($this->uid);
         }
+        // TODO 前端登陆冲突
+        if (!empty(session('admin')))
+        {
+            header("Location: " . U('login/index'));
+            exit();
+        }
+        // 前端登陆冲突
+
         if (strtolower(MODULE_NAME) != 'login' && strtolower(MODULE_NAME) != 'public') { //public 不受权限控制
             if (empty($this->uid)) {
                 header("Location: " . U('login/index'));
